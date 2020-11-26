5b9b7122162d3ba91b239975ed4ef971d0433bf2
Rongsong Shen <shenrs@cn.ibm.com>
2020-11-26 22:28:40 +0800

    add squid

--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,32 @@
 #! /bin/sh
 
+mk_squid_pam() {
+cat >/etc/pam.d/squid <<EOF
+auth required pam_unix.so
+account required pam_unix.so
+EOF
+}
+
+fix_helper_perm() {
+   chmod a+s /usr/local/libexec/squid/basic_pam_auth
+}
+
+mk_users() {
+  for _user in `echo $proxy_users`; do
+      pw user add $_user -m
+      _z1=`echo $_user passwd | sed 's/ /_/g'`
+      _z2="echo \$$_z1"
+      _passwd=`eval $_z2`
+      /root/chpasswd $_user $_passwd
+  done
+}
+
+if [ -f /root/proxy.conf ]; then
+   . /root/proxy.conf
+fi
+
+mk_squid_pam
+fix_helper_perm
+
 sysrc squid_enable="YES"
 service squid start
